f4eb5946243a9a33e4dc9ccbbeccc6289a94d8bb
root <root@DESKTOP-OSI4R81>
2024-09-20 16:27:43 -0400

    add madlib

---
+++ my-work/madlib.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Name an unusual university class: " NOUN1
+read -p "2. Name a place to go on grounds: " NOUN2
+read -p "3. Name the verb that your favorite animal does to get around: " VERB1
+read -p "4. Name an unusual name: " NOUN3
+read -p "5. Name a rude descriptive adjective: " ADJECTIVE1
+read -p "6. Name an unusual food: " NOUN4
+read -p "7. Name an object: " NOUN5
+read -p "8. Name your favorite dessert: " NOUN6
+
+
+clear 
+
+echo "Hank was not happy. He missed his enrollment appointment and was forced to take $NOUN1." 
+echo "It was located in $NOUN2 which was super far from his place!" 
+echo "The professor's name was $NOUN3 and Hank had heard that he was a little bit $ADJECTIVE1." 
+echo "He also heard that he smelled like $NOUN4." 
+echo "Later that day, Hank was $VERB1 to class but suddenly he tripped over a $NOUN5!"
+echo "Somebody caught him. That person was no other than Professor $NOUN3."
+echo "Filled with gratitude, Hank felt guilty for thinking all those mean things." 
+echo "Professor $NOUN3 looked kind and smelled like $NOUN6."
+echo "Hank now remembers to never trust rumors."
+
+
+
+
